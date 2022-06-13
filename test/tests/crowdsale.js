bc0ea603230c2f9469823ca7bf8da47b007b5777
nuwrldnf8r <nuwrldnf8r@gmail.com>
2022-06-13 07:42:43 +0200

    Update crowdsale.js

--- test/tests/crowdsale.js
+++ test/tests/crowdsale.js
@@ -1,14 +1,17 @@
 const ethers = require('ethers')
 const abi = require('../lib/abi')
 const bytecode = require('../lib/bytecode')
-const assert = require('assert');
-const privateKeys = require('../lib/pks');
-const { forEach } = require('../lib/pks');
+//const assert = require('assert');
+const privateKeys = require('../lib/pks')
+const { forEach } = require('../lib/pks')
+const { exit } = require('process');
+const cliProgress = require('cli-progress')
 
 const rpcEndPoint = 'https://xapi.testnet.fantom.network/lachesis'
 const provider = new ethers.providers.JsonRpcProvider(rpcEndPoint)
 
 let crowdsaleAddress = ''
+let toaAddress = ''
 
 let users = {
     owner: privateKeys[0],
@@ -37,7 +40,7 @@ let allocIDX = {
 }
 
 let allocation = {
-    benificiaries: 60,
+    beneficiaries: 60,
     guardian: 25,
     rainmaker: 2,
     bonus: 5,
@@ -50,6 +53,20 @@ let fundPerc = {
     platform: 3,
     reserve: 3,
     auditor: 3
+}
+
+const assert = (condition,msg) => {
+    if(!condition){
+        console.log(msg)
+        exit()
+    }
+    return
+}
+
+const numTOAs = () => {
+    let num = 0
+    Object.keys(allocation).forEach(a=>num+=allocation[a])
+    return num
 }
 
 const ownerWallet = new ethers.Wallet(users.owner,provider)
@@ -107,7 +124,7 @@ const deploy = async () => {
         let numTOAs = 100
         let price = toBN(1000,6)        
         let toaAlloc = [
-            allocation.benificiaries,
+            allocation.beneficiaries,
             allocation.guardian,
             allocation.rainmaker,
             allocation.bonus,
@@ -123,18 +140,19 @@ const deploy = async () => {
         const contract = await factory.deploy(
             usdcAddress,
             toaAddress,
-            toBN(10).mul(price),
-            toBN(300),
+            toBN(8).mul(price),
+            toBN(60),
             price,
             toaAlloc,
             fundAlloc,
-            toBN(420)
+            toBN(80)
         );
         await confirm(contract.deployTransaction.hash)
         return {toaAddress: toaAddress, crowdsaleAddress: contract.address}
     }
     catch(e){
         console.log(e)
+        exit()
     }
     return null
 }
@@ -162,11 +180,24 @@ const start = async () => {
     }
     catch(e){
         console.log(e)
+        exit()
     }
 }
 
 const wait = (n) => {
-    return new Promise(r=>setTimeout(()=>r(),n))
+    const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+    bar1.start(n/1000, 0);
+    let tm = 0
+    let interval = setInterval(()=>{
+        tm+=1
+        bar1.update(tm)
+    },1000)
+    return new Promise(r=>setTimeout(()=>{
+        clearInterval(interval)
+        bar1.update(n/1000)
+        bar1.stop()
+        r()
+    },n))
 }
 
 const mintUSDC = async (user, amount) => {
@@ -188,32 +219,37 @@ const checkTOABalance = async (user) => {
     return bal
 }
 
-const checkInitialTOABalances = async () => {
-    let allocation = {
-        benificiaries: 60,
-        guardian: 25,
-        rainmaker: 2,
-        bonus: 5,
-        concierge: 5,
-        wisdom: 21
-    }
+const checkTOABalances = async (remainder) => {
+    console.log('remainder = ' + remainder)
     console.log('checking initial TOA balances')
-    let checkTOABal = async (user) => {
+    let checkTOABal = async (user,checkRemainder) => {
         try{
             let bal = await checkTOABalance(users[user])
-            assert(bal.eq(allocation[user]),user + " balance incorrect. = " + bal + ',should be ' + allocation[user])
+            let alloc = toBN(allocation[user])
+            if(checkRemainder) alloc = alloc.add(toBN(remainder))
+            console.log('bal + remainder: ' + bal)
+            assert(bal.eq(alloc),user + " balance incorrect. = " + bal + ',should be ' + alloc)
         }
         catch(e){
             assert(false,e.message)
         }
     }
+    
+    
     await Promise.all([
         checkTOABal('guardian'),
         checkTOABal('rainmaker'),
-        checkTOABal('bonus'),
+        checkTOABal('bonus',true),
         checkTOABal('concierge'),
         checkTOABal('wisdom')
     ])
+    /*
+    await checkTOABal('guardian')
+    await checkTOABal('rainmaker')
+    await checkTOABal('bonus',true) //,remainder),
+    await checkTOABal('concierge')
+    await checkTOABal('wisdom')
+    */
 }
 
 const setAllocationAddress = async (user) => {
@@ -223,6 +259,17 @@ const setAllocationAddress = async (user) => {
     const contract = new ethers.Contract(crowdsaleAddress, abi.crowdsale, ownerWallet)
     let ret = await contract.setAllocationAddress(idx,userWallet.address)
     await confirm(ret.hash)
+    
+    /*
+    console.log('checking assignments')
+    let _idx = await contract.assignmentIdx(userWallet.address)
+    assert(idx.eq(_idx),'Incorrect idx. Is ' + _idx + ', should be ' + idx)
+    let address = await contract.assignmentAddress(idx)
+    assert(address==userWallet.address,'Incorrect address. Is ' + address + ',should be ' + userWallet.address)
+    let isAcc = await contract.isAssigned(userWallet.address)
+    assert(isAcc,'Account not asigned')
+    */
+    
 }
 
 const setAllocationAddresses = async () => {
@@ -237,8 +284,34 @@ const setAllocationAddresses = async () => {
     await setAllocationAddress('wisdom')
 }
 
+const getTOABalance = async (address) => {
+    const contract = new ethers.Contract(toaAddress, abi.toa, ownerWallet)
+    return await contract.balanceOf(address)
+}
+
+const assignTOA = async (pk,user) => {
+    let userWallet = new ethers.Wallet(pk,provider)
+    const contract = new ethers.Contract(crowdsaleAddress, abi.crowdsale, userWallet)
+    user = user || userWallet.address
+    console.log('..assigning TOA to ' + user)
+    try{
+        let available = await checkTOABalance(pk)
+        let ret = await contract.assignTOAs(userWallet.address)
+        await confirm(ret.hash)
+        let bal = await getTOABalance(userWallet.address)
+        console.log('Balance for ' + user + ': ' + bal)
+        assert(bal.eq(available),'incorrect balance for ' + user)
+        bal = await checkTOABalance(pk)
+        assert(bal.eq(toBN(0)),'balance should be zero')
+    }
+    catch(e){
+        assert(false,e.message)
+    }
+}
+
+
 const buy = async (user, num, checkBalBefore) => {
-    console.log('buying ' + num + 'TOA' + ((num>1)?'s':''))
+    console.log('buying ' + num + ' TOA' + ((num>1)?'s':''))
     let amnt = toBN(1000,6).mul(num)
     let numTOAs = toBN(num)
     const userWallet = new ethers.Wallet(user,provider)
@@ -264,20 +337,155 @@ const buy = async (user, num, checkBalBefore) => {
 
 }
 
-
-
-const run = async () => {
-   let ret = await deploy()
-   assert((ret),"Deploy Failed")
-   await changeTOAOwner(ret.toaAddress, ret.crowdsaleAddress)
-   crowdsaleAddress = ret.crowdsaleAddress
-   await start()
-   await buy(users.beneficiaries[0],1)
-   await buy(users.beneficiaries[0],2,true)
-   //await setAllocationAddresses()
-   //await checkInitialTOABalances()
-   
-   return 'done'
+const succesfulCampaign = async (buyers) => {
+    console.log('*simulating succesful campaign*')
+    let sold = 0
+    await buy(buyers[0].buyer,buyers[0].amount)
+    sold += buyers[0].amount
+    //await buy(users.beneficiaries[0],1,true)
+    //await buy(users.beneficiaries[2],5)
+    const contract = new ethers.Contract(crowdsaleAddress, abi.crowdsale, ownerWallet)
+    let tm = await contract.timeUntilEnd()
+    console.log('time to end: ' + tm)
+    tm = tm.add(toBN(20))
+    console.log('waiting ' + tm + ' secs')
+    await wait(tm.toNumber()*1000)
+    try{
+        let isSuccess = await contract.isSuccess()
+        assert(isSuccess, 'campaign failed :(')
+        console.log('campaign succesful')
+    }
+    catch(e){
+        assert(false,e.message)
+    }
+    return sold
 }
 
-run().then(console.log)
+const failCampaign = async (buyers) => {
+    console.log('*simulating failure campaign*')
+    let sold = 0
+    await buy(buyers[0].buyer,buyers[0].amount)
+    sold += buyers[0].amount
+    const contract = new ethers.Contract(crowdsaleAddress, abi.crowdsale, ownerWallet)
+    let tm = await contract.timeUntilEnd()
+    console.log('time to end: ' + tm)
+    tm = tm.add(toBN(20))
+    console.log('waiting ' + tm + ' secs')
+    await wait(tm.toNumber()*1000)
+    try{
+        let isSuccess = await contract.isSuccess()
+        assert(!isSuccess,'Campain should have failed')
+    }
+    catch(e){
+        assert(true,'Campain should have failed')
+    }
+    console.log('Campaign failed')
+    return sold
+}
+
+const USDCBal = async (address) => {
+    const contract = new ethers.Contract(usdcAddress, abi.usdc, ownerWallet)
+    return await contract.balanceOf(address)
+}
+
+const fundsRaised = async () => {
+    console.log('getting funds raised')
+    const contract = new ethers.Contract(crowdsaleAddress, abi.crowdsale, ownerWallet)
+    let funds = await contract.fundsRaised()
+    let bal = await USDCBal(crowdsaleAddress)
+    assert(funds.eq(bal),'imbalance in funds raised vs actual balance')
+    console.log('..funds raised: ' + funds)
+    return funds
+}
+
+const assignFunds = async (user, totalFunds) => {
+    
+    console.log('assigning funds to ' + user)
+    const userWallet = new ethers.Wallet(users[user],provider)
+    const contract = new ethers.Contract(crowdsaleAddress, abi.crowdsale, userWallet)
+    let expectedBalance = toBN(fundPerc[user]).mul(totalFunds).div(toBN(100))
+    let bal = await contract.balanceOf(userWallet.address)
+    assert(bal.eq(expectedBalance),'Unexpected balance. Is ' + bal + ', sould be ' + expectedBalance)
+    let userBal = await USDCBal(userWallet.address)
+    try{
+        let ret = await contract.withdrawFunds(userWallet.address)
+        await confirm(ret.hash)
+        let newBalance = await USDCBal(userWallet.address)
+        assert(userBal.add(expectedBalance).eq(newBalance),'Balance incorrect. Is ' + newBalance + ', should be ' + (userBal.add(expectedBalance)))
+    }
+    catch(e){
+        console.log(e.message)
+        exit()
+    }
+}
+
+const returnFunds = async(buyer) => {
+    console.log('returning funds to ' + buyer.buyer)
+    const userWallet = new ethers.Wallet(buyer.buyer,provider)
+    const contract = new ethers.Contract(crowdsaleAddress, abi.crowdsale, userWallet)
+    let amnt = toBN(1000,6).mul(toBN(buyer.amount))
+    let userBal = await USDCBal(userWallet.address)
+    try{
+        let ret = await contract.returnFunds(userWallet.address)
+        await confirm(ret.hash)
+        let newBal = await USDCBal(userWallet.address)
+        assert(userBal.add(amnt).eq(newBal),"Incorrect balance. Got " + newBal + ', should be ' + userBal.add(amnt))
+    }
+    catch(e){
+        assert(false,e.message)
+    }
+    return
+}
+
+const runSuccess = async () => {
+    let ret = await deploy()
+    assert((ret),"Deploy Failed")
+    await changeTOAOwner(ret.toaAddress, ret.crowdsaleAddress)
+    crowdsaleAddress = ret.crowdsaleAddress
+    console.log('crowdsale contract address: ' + crowdsaleAddress)
+    toaAddress = ret.toaAddress
+    
+    await start()
+    let buyers = [
+        {buyer: users.beneficiaries[0], amount: 9}
+    ]
+    let numSold = await succesfulCampaign(buyers)
+    await setAllocationAddresses()
+    //await checkTOABalances(allocation['beneficiaries']-numSold)
+    
+    console.log('assigning toas')
+    await assignTOA(buyers[0].buyer)
+    await assignTOA(users['guardian'],'guardian')
+    await assignTOA(users['rainmaker'],'rainmaker')
+    await assignTOA(users['bonus'],'bonus')
+    await assignTOA(users['concierge'],'concierge')
+    await assignTOA(users['wisdom'],'wisdom')
+
+    let funds = await fundsRaised()
+    await assignFunds('producer',funds)
+    await assignFunds('platform',funds)
+    await assignFunds('reserve',funds)
+    await assignFunds('auditor',funds)
+    
+    return 'done'
+    
+}
+
+const runFail = async () => {
+    let ret = await deploy()
+    assert((ret),"Deploy Failed")
+    await changeTOAOwner(ret.toaAddress, ret.crowdsaleAddress)
+    crowdsaleAddress = ret.crowdsaleAddress
+    console.log('crowdsale contract address: ' + crowdsaleAddress)
+    toaAddress = ret.toaAddress
+    
+    await start()
+    let buyers = [
+        {buyer: users.beneficiaries[0], amount: 7}
+    ]
+    await failCampaign(buyers)
+    await returnFunds(buyers[0])
+    return 'done'
+}
+
+runFail().then(console.log)
