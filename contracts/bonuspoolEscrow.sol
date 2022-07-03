d088ecd3c3df73eab1a91098b1ed20c86f65984b
nuwrldnf8r <nuwrldnf8r@gmail.com>
2022-07-03 11:50:13 +0200

    Update bonuspoolEscrow.sol

--- contracts/bonuspoolEscrow.sol
+++ contracts/bonuspoolEscrow.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract GuardianEscrow{
+contract BonusPoolEscrow{
 
     address private _nft;
     address private _executionContract;
